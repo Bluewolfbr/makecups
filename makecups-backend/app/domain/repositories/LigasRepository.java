32b9219ca0b1eb7d12e4400607c407898abdde25
Onezino Gabriel <zinogabriel@gmail.com>
2015-02-18 11:44:54 -0200

    nova assinatura para o repository da liga fornecer um builder
    coloca a liga como valores finais para fixa-los e transforma em um value
    object

--- makecups-backend/app/domain/repositories/LigasRepository.java
+++ makecups-backend/app/domain/repositories/LigasRepository.java
@@ -1,7 +1,10 @@
 package domain.repositories;
 
+import ddd.easy.Builder;
+import ddd.easy.Factory;
 import ddd.easy.Repository;
 import domain.models.Liga;
+import domain.models.campeonato.CampeonatoBuilder;
 
 import java.util.List;
 
@@ -11,4 +14,10 @@ public interface LigasRepository extends Repository<Liga>{
     public Liga consutar(Long id);
 
     public List<Liga> todos();
+
+    public LigaBuilder build(String nome, String pais, String regiao);
+
+    public interface LigaBuilder extends Builder<Liga>{
+            public Liga build();
+    }
 }
